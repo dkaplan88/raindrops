b7859ac5bf9dc508843f3f9c436405e0aa2ca581
dkaplan <dkaplan@enova.com>
2014-05-05 00:53:39 -0500

    one liners

--- raindrops.rb
+++ raindrops.rb
@@ -2,16 +2,10 @@ class Raindrops
   PRIMES = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
 
   def self.convert(number)
-    if prime_factors_for(number).join.empty? 
-      number.to_s 
-    else
-      prime_factors_for(number).join
-    end
+    prime_factors_for(number).any? ? prime_factors_for(number).join : number.to_s
   end
 
   def self.prime_factors_for(number)
-    PRIMES.map do |prime, output|
-      output if number.modulo(prime).zero?
-    end
+    PRIMES.map { |prime, output| output if number.modulo(prime).zero? }
   end
 end
